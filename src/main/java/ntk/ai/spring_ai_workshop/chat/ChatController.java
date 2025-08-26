36a9f1c84bd6dc12dfd492671a2e066b08085060
1114nikhil <nikhiltk1114@gmail.com>
2025-08-26 06:05:23 +0530

    chat working

--- src/main/java/ntk/ai/spring_ai_workshop/chat/ChatController.java
+++ src/main/java/ntk/ai/spring_ai_workshop/chat/ChatController.java
@@ -1,8 +1,10 @@
 package ntk.ai.spring_ai_workshop.chat;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class ChatController {
@@ -18,5 +20,21 @@ public class ChatController {
                 .user("Tell me Interesting fact about Java!")
                 .call()
                 .content();
+    }
+
+    @GetMapping("stream")
+    public Flux<String> stream(){
+        return chatClient.prompt()
+                .user("I am Visiting Kerala soon, can you give me 10 places I must visit?")
+                .stream()
+                .content();
+    }
+
+    @GetMapping("joke")
+    public ChatResponse joke(){
+        return chatClient.prompt()
+                .user("Tell me coding Joke")
+                .call()
+                .chatResponse();
     }
 }
