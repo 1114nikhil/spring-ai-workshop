72c68cb2b743ad090e9828ea6cec376308a98ee2
1114nikhil <nikhiltk1114@gmail.com>
2025-08-26 05:40:46 +0530

    env test

---
+++ src/main/java/ntk/ai/spring_ai_workshop/chat/ChatController.java
@@ -0,0 +1,22 @@
+package ntk.ai.spring_ai_workshop.chat;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("chat")
+    public String chat(){
+        return chatClient.prompt()
+                .user("Tell me Interesting fact about Java!")
+                .call()
+                .content();
+    }
+}
