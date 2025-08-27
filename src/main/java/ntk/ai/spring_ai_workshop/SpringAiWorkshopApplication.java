e21c3e8a1ecedaa70128d290011a6b2ba31f1931
1114nikhil <nikhiltk1114@gmail.com>
2025-08-27 06:36:04 +0530

    work with multiple chat AI model

--- src/main/java/ntk/ai/spring_ai_workshop/SpringAiWorkshopApplication.java
+++ src/main/java/ntk/ai/spring_ai_workshop/SpringAiWorkshopApplication.java
@@ -1,7 +1,13 @@
 package ntk.ai.spring_ai_workshop;
 
+import org.springframework.ai.anthropic.AnthropicChatModel;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @SpringBootApplication
 public class SpringAiWorkshopApplication {
@@ -9,5 +15,20 @@ public class SpringAiWorkshopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAiWorkshopApplication.class, args);
 	}
+
+	@Bean
+	@Primary //to use default one
+	@Qualifier("openAIChatClient") //to match with the controller where we want to use
+	public ChatClient openAIChatClient(OpenAiChatModel chatModel){
+		return ChatClient.create(chatModel);
+	}
+//
+//	because I don't have API key
+//	@Bean
+//	@Qualifier("anthropicChatClient")
+//	public ChatClient anthropicChatClient(AnthropicChatModel chatModel){
+//		return ChatClient.create(chatModel);
+//	}
+
 
 }
---
+++ src/main/java/ntk/ai/spring_ai_workshop/chat/AnthropicChatClient.java
@@ -0,0 +1,15 @@
+package ntk.ai.spring_ai_workshop.chat;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AnthropicChatClient {
+
+//    private final ChatClient chatClient;
+//
+//    public AnthropicChatClient(@Qualifier("anthropicChatClient") ChatClient chatClient) {
+//        this.chatClient = chatClient;
+//    }
+}
